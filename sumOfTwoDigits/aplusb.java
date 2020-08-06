a8197c27580132731ec58d716ed3934ffe934c0e
Rishabh <rishabh.kumar@medlife.com>
2020-08-06 14:52:23 +0530

    sumOfTwoDigits

---
+++ sumOfTwoDigits/aplusb.java
@@ -0,0 +1,11 @@
+package coursera.algorithmictoolbox.sumoftwodigits;
+import java.util.*;
+
+public class aplusb {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a + b);
+	}
+}
